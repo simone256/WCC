3cd8ac401bd0e88ae2d6f807200fcb085cc69e01
simone256 <victoriadonovan@Victorias-MBP-3.home>
2016-10-29 17:44:25 -0400

    Variables Code Challenge

---
+++ python/variables.py
@@ -0,0 +1,10 @@
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print("Ariel is " + str(age) + " years old now, but she'll turn " + str(next_age) + " in " + birth_month + '.')
+print("Her birth stone is " + birth_stone + '.')
+
+#Ariel is 30 years old now, but she'll turn 31 in August.
+#Her birth stone is peridot.
