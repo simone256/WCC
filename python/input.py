b2d0efa4d3f39529b2713a60b25b93ea9eb4eb3a
simone256 <victoriadonovan@Victorias-MBP-3.home>
2016-10-29 20:18:07 -0400

    User Input Challenge

---
+++ python/input.py
@@ -0,0 +1,16 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you?')
+#dog_years = int(age) * 7
+#print('You are ' + dog_years + 'old in dog years.')
+
+price = raw_input('How much was your meal?')
+tip = float(price) * .20
+total_price = float(price) + float(tip)
+print('You should tip $' + str(tip))
+print('Your total cost would be $' + str(total_price))
